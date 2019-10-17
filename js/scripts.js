208bc1543043bae85176a6aa3fbcf2a0db297446
Ariska28 <55794499+Ariska28@users.noreply.github.com>
2019-10-17 19:25:34 +0300

    efhn

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $().ready(function () {
     $("#Click").on("click", function() {
         $(".menu").toggleClass("menu--disable" ) &  
-        $(".header__logotype").toggleClass("header__logotype--color" );
+        $(".header-logotype").toggleClass("header-logotype--color" );
 }) 
     });
