faaabfae5831754bc5329c9e339a142d297256d8
Ariska28 <55794499+Ariska28@users.noreply.github.com>
2019-10-17 18:44:34 +0300

    BAM

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $().ready(function () {
     $("#Click").on("click", function() {
-        $(".main__menu").toggleClass("main__menu-disable" ) &  $(".header__logotype").toggleClass("header__logotype-color" );
+        $(".menu").toggleClass("menu--disable" ) &  
+        $(".header__logotype").toggleClass("header__logotype--color" );
 }) 
     });
